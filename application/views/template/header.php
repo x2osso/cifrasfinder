ae18772c14e316b5aaa4ff58236590f7872ca6de
Gabriel <x2osso123@gmail.com>
2024-02-04 01:14:18 -0300

    ATT template novo

---
+++ application/views/template/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Pod Talk - Free Bootstrap 5 CSS Template</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url();?>public/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>public/css/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>public/css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>public/css/owl.theme.default.min.css">
+
+    <link href="<?= base_url();?>public/css/templatemo-pod-talk.css" rel="stylesheet">
+
+    <!--
+
+TemplateMo 584 Pod Talk
+
+https://templatemo.com/tm-584-pod-talk
+
+-->
+</head>
---
+++ application/views/template/scripts.php
@@ -0,0 +1,5 @@
+<!-- JAVASCRIPT FILES -->
+<script src="<?= base_url(); ?>public/js/jquery.min.js"></script>
+<script src="<?= base_url(); ?>public/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>public/js/owl.carousel.min.js"></script>
+<script src="<?= base_url(); ?>public/js/custom.js"></script>
